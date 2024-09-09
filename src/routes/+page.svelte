e3448631555066eed65f6184723f7f7a91793b84
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-09-09 11:46:03 +0200

    feat: first version, entity boxes and summary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,55 @@
 <script lang="ts">
-	import '../styles.css';
-	import Chunk from './Chunk.svelte';
-	import { page } from '$app/stores';
+    import "../styles.css";
+    import Chunk from "./Chunk.svelte";
+    import { page } from "$app/stores";
 
-	const pmid = $page.url.searchParams.get('pmid');
-	const start = $page.url.searchParams.get('start');
+    const annotator = $page.url.searchParams.get("annotator");
+    const id = $page.url.searchParams.get("id");
+
+    let entities: { type: string; text: string }[] = [];
+    let strains: { type: string; text: string }[] = [];
+    let species: { type: string; text: string }[] = [];
+
+    function handleEntityFound(event: CustomEvent) {
+        const { type, text } = event.detail;
+        entities = [...entities, { type, text }];
+        strains = entities.filter(
+            (entity) => entity.type == "ncbitaxon:Strain",
+        );
+        species = entities.filter(
+            (entity) => entity.type == "ncbitaxon:Species",
+        );
+    }
 </script>
 
-<div>
-	<Chunk {pmid} {start} />
+<div id="container">
+    <div id="chunk">
+        <Chunk {annotator} {id} on:entityFound={handleEntityFound} />
+    </div>
+
+    <div id="summary">
+        <h2>Summary</h2>
+
+        {#if entities.length > 0}
+            {#if strains.length > 0}
+                <h3>Strains</h3>
+                {#each strains as entity}
+                    <span class="entitySummary" typeof={entity.type}>
+                        {entity.text}
+                    </span>
+                {/each}
+            {/if}
+
+            {#if species.length > 0}
+                <h3>Species</h3>
+                {#each species as entity}
+                    <span class="entitySummary" typeof={entity.type}>
+                        {entity.text}
+                    </span>
+                {/each}
+            {/if}
+        {:else}
+            <p>No entities found.</p>
+        {/if}
+    </div>
 </div>
--- src/routes/Chunk.svelte
+++ src/routes/Chunk.svelte
@@ -1,42 +1,62 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	export let pmid: string = '';
-	export let start: string = '';
-	let promise;
+    import { onMount, createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    export let annotator: string = "";
+    export let id: string = "";
+    let promise;
 
-	async function loadChunk() {
-		let url: string;
-		if (start && pmid) {
-			console.log(`loading position ${start} from article ${pmid}`);
-			url = `http://localhost:8000/segment/?pmid=${pmid}&start=${start}`;
-		} else if (pmid) {
-			console.log(`loading article ${pmid}`);
-			url = `http://localhost:8000/segment/?pmid=${pmid}`;
-		} else {
-			console.log('start and pmid are null');
-			url = 'http://localhost:8000/segment/';
-		}
-		let response = await fetch(url);
-		return response.json();
-	}
+    async function loadChunk() {
+        let url: string;
+        if (annotator && id) {
+            url = `http://localhost:8000/annotation/?annotator=${annotator}&id=${id}`;
+        } else if (id) {
+            console.log(`loading article ${id}`);
+            url = `http://localhost:8000/segment/?pmid=${id}`;
+        } else {
+            console.log("start and pmid are null");
+            url = "http://localhost:8000/segment/";
+        }
+        let response = await fetch(url);
+        return response.json();
+    }
 
-	function log(str: string) {
-		console.log('Response data:', JSON.stringify(str, null, 2));
-	}
+    function log(str: string) {
+        console.log("Response data:", JSON.stringify(str, null, 2));
+    }
 
-	onMount(() => {
-		promise = loadChunk();
-	});
+    function processTags(content: string) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(content, "text/html");
+        const spans = doc.querySelectorAll("span[typeof]");
+
+        spans.forEach((span) => {
+            const type = span.getAttribute("typeof");
+            const text = span.textContent;
+            dispatch("entityFound", { type, text });
+        });
+
+        return content;
+    }
+
+    onMount(() => {
+        promise = loadChunk().then((data) => {
+            if (data) {
+                const content = JSON.parse(data).content;
+                return processTags(content);
+            }
+            return null;
+        });
+    });
 </script>
 
 {#await promise}
-	<p>Loading...</p>
-{:then data}
-	<div>
-		{#if data}
-			{@html JSON.parse(data).content}
-		{/if}
-	</div>
+    <p>Loading...</p>
+{:then content}
+    <div>
+        {#if content}
+            {@html content}
+        {/if}
+    </div>
 {:catch error}
-	<p style="color: red">{error.message}</p>
+    <p style="color: red">{error.message}</p>
 {/await}
