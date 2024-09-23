cf01f16795bc9a21be37e844015c51ef1cfbd10f
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-09-23 17:31:00 +0200

    feat: add handlers for mouse events

---
+++ src/lib/handlers.ts
@@ -0,0 +1,143 @@
+import {
+    resources,
+    sameClass,
+    isStrain,
+    isBacteria,
+    isOrganism,
+    isEnzyme,
+} from "$lib/resources.ts";
+import type { Resource } from "$lib/resources.ts";
+import { rangeToClass, trimRange } from "../ranges.ts";
+
+let selectedText = "";
+let selectedSpan: HTMLSpanElement | null = null;
+let sourceRes: Resource | null;
+let targetRes: Resource | null;
+export let showDropdown = false;
+export let showRemoveDropdown = false;
+export let dropdownPosition = { x: 0, y: 0 };
+
+function processTags(element: HTMLElement): void {
+    const spans = element.querySelectorAll("span[typeof]");
+
+    spans.forEach((span) => {
+        resources.storeEntitySpan(span);
+    });
+}
+
+export function handleTextSelection(event: MouseEvent) {
+    const selection = window.getSelection();
+    if (selection && !selection.isCollapsed) {
+        selectedText = selection.toString().trim();
+        if (selectedText) {
+            setOptionsDropdown(event);
+        }
+    } else {
+        showDropdown = false;
+    }
+}
+
+export function setOptionsDropdown(event: MouseEvent) {
+    showDropdown = true;
+    showRemoveDropdown = false;
+    dropdownPosition = { x: event.clientX, y: event.clientY };
+}
+
+export function handleSpanClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("entity")) {
+        event.stopPropagation();
+        selectedSpan = target as HTMLSpanElement;
+        showRemoveDropdown = true;
+        showDropdown = false;
+        dropdownPosition = { x: event.clientX, y: event.clientY };
+        setOptionsDropdown(event);
+    } else {
+        showRemoveDropdown = false;
+    }
+}
+
+export function handleOptionClick(option: string) {
+    const selection = window.getSelection();
+    if (selection && !selection.isCollapsed) {
+        const range = trimRange(selection.getRangeAt(0));
+        const span = document.createElement("span");
+        const label = `d3o:${option}`;
+        span.className = rangeToClass(range);
+        span.setAttribute("typeof", label);
+        range.surroundContents(span);
+        selection.removeAllRanges();
+        resources.storeEntitySpan(span);
+    }
+    showDropdown = false;
+}
+
+export function handleRemoveAnnotation() {
+    if (selectedSpan) {
+        const parent = selectedSpan.parentNode;
+        removeEntitySpan(selectedSpan);
+        if (parent) {
+            while (selectedSpan.firstChild) {
+                parent.insertBefore(selectedSpan.firstChild, selectedSpan);
+            }
+            parent.removeChild(selectedSpan);
+        }
+    }
+    showRemoveDropdown = false;
+}
+
+export function handleDrop(e: DragEvent): void {
+    targetRes = resourceFromTarget(e.target);
+    if (sourceRes && targetRes) {
+        if (sameClass(sourceRes, targetRes)) {
+            resources.merge(sourceRes, targetRes);
+        } else if (isStrain(sourceRes) && isBacteria(targetRes)) {
+            relations.add(sourceRes, "strainOf", targetRes);
+        } else if (isBacteria(sourceRes) && isStrain(targetRes)) {
+            relations.add(targetRes, "strainOf", sourceRes);
+        } else if (isEnzyme(sourceRes) && isOrganism(targetRes)) {
+            relations.add(targetRes, "has", sourceRes);
+        } else if (isOrganism(sourceRes) && isEnzyme(targetRes)) {
+            relations.add(sourceRes, "has", targetRes);
+        }
+    }
+}
+
+function resourceFromTarget(target: EventTarget | null): Resource | null {
+    if (target && target instanceof Element) {
+        const id = target.getAttribute("resource");
+        const label = target.getAttribute("typeof");
+
+        if (id && label) {
+            return { id: id, label: label };
+        }
+    }
+    return null;
+}
+
+export function getOptions(): Array<string> {
+    const options: Array<string> = [];
+    if (sourceRes && targetRes) {
+        if (isStrain(sourceRes) && isBacteria(targetRes)) {
+            options.push("is strain of");
+        }
+        if (isBacteria(sourceRes) && isStrain(targetRes)) {
+            options.push("is superordinate of");
+        }
+        if (isEnzyme(sourceRes) && isOrganism(targetRes)) {
+            options.push("is found in");
+        }
+        if (isOrganism(sourceRes) && isEnzyme(targetRes)) {
+            options.push("has");
+        }
+    }
+    return options;
+}
+
+export function dragStart(e: DragEvent): void {
+    sourceRes = resourceFromTarget(e.target);
+}
+
+export function dragOver(e: DragEvent): void {
+    e.preventDefault();
+}
--- src/routes/ChunkBody.svelte
+++ src/routes/ChunkBody.svelte
@@ -1,78 +1,24 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { removeEntitySpan, storeEntitySpan } from "./resources.ts";
-    import { rangeToClass, trimRange } from "../ranges.ts";
+  import { onMount } from "svelte";
+  import { resources } from "$lib/resources.ts"
+    import {
+        showDropdown,
+        showRemoveDropdown,
+        dropdownPosition,
+        handleOptionClick,
+        handleSpanClick,
+        handleTextSelection,
+        handleRemoveAnnotation,
+    } from "$lib/handlers.ts";
 
-    export let body: HTMLDivElement | null;
+  export let body: HTMLDivElement | null;
 
-    let selectedText = "";
-    let selectedSpan: HTMLSpanElement | null = null;
-    let showDropdown = false;
-    let showRemoveDropdown = false;
-    let dropdownPosition = { x: 0, y: 0 };
-
-    function processTags(element: HTMLElement): void {
-        const spans = element.querySelectorAll("span[typeof]");
+  function processTags(content: HTMLDivElement) {
+        const spans = content.querySelectorAll("span[typeof]");
 
         spans.forEach((span) => {
-            storeEntitySpan(span);
+          resources.storeEntitySpan(span);
         });
-    }
-
-    function handleTextSelection(event: MouseEvent) {
-        const selection = window.getSelection();
-        if (selection && !selection.isCollapsed) {
-            selectedText = selection.toString().trim();
-            if (selectedText) {
-                showDropdown = true;
-                showRemoveDropdown = false;
-                dropdownPosition = { x: event.clientX, y: event.clientY };
-            }
-        } else {
-            showDropdown = false;
-        }
-    }
-
-    function handleSpanClick(event: MouseEvent) {
-        const target = event.target as HTMLElement;
-        if (target.classList.contains("entity")) {
-            event.stopPropagation();
-            selectedSpan = target as HTMLSpanElement;
-            showRemoveDropdown = true;
-            showDropdown = false;
-            dropdownPosition = { x: event.clientX, y: event.clientY };
-        } else {
-            showRemoveDropdown = false;
-        }
-    }
-
-    function handleOptionClick(option: string) {
-        const selection = window.getSelection();
-        if (selection && !selection.isCollapsed) {
-            const range = trimRange(selection.getRangeAt(0));
-            const span = document.createElement("span");
-            const label = `d3o:${option}`;
-            span.className = rangeToClass(range);
-            span.setAttribute("typeof", label);
-            range.surroundContents(span);
-            selection.removeAllRanges();
-            storeEntitySpan(span);
-        }
-        showDropdown = false;
-    }
-
-    function handleRemoveAnnotation() {
-        if (selectedSpan) {
-            const parent = selectedSpan.parentNode;
-            removeEntitySpan(selectedSpan);
-            if (parent) {
-                while (selectedSpan.firstChild) {
-                    parent.insertBefore(selectedSpan.firstChild, selectedSpan);
-                }
-                parent.removeChild(selectedSpan);
-            }
-        }
-        showRemoveDropdown = false;
     }
 
     onMount(() => {
