5748b497d3c22f4eaea06a41413c981974ac3d9d
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-09-23 17:19:43 +0200

    test: set up vitest to flag slow tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), svelteTesting()],
+    test: {
+        globals: true,
+        slowTestThreshold: 1,
+        bail: 1,
+    },
 });
