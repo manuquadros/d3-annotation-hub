8fb3a254b246a27b3242a2e21a2b5562641ff7dd
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-09-24 13:26:32 +0200

    fix: update the Summary component to use the new resource store

--- src/routes/Summary.svelte
+++ src/routes/Summary.svelte
@@ -1,30 +1,70 @@
 <script lang="ts">
-    import { resources } from "$lib/resources.ts";
-    function plural(singular: string): string {
-        if (singular === "Bacteria") {
-            return singular;
-        } else {
-            return singular + "s";
-        }
+  import {
+    bacteriaLabel,
+    enzymeLabel,
+    strainLabel,
+    resources,
+  } from "$lib/resources.ts";
+  import { derived, get } from "svelte/store";
+  import { dragStart, dragOver, handleDrop } from "$lib/handlers.ts";
+
+  function plural(singular: string): string {
+    if (singular === "Bacteria") {
+      return singular;
+    } else {
+      return singular + "s";
     }
+  }
+
+  const classLabels = [enzymeLabel, strainLabel, bacteriaLabel];
+
+  const classes = derived(resources, ($resources) => {
+    const map = new Map();
+    classLabels.forEach((label) =>
+      map.set(
+        label,
+        Array.from($resources.values()).filter((res) => res.label === label),
+      ),
+    );
+
+    return map;
+  });
+
+  console.log($classes);
 </script>
 
 <h2>Entities</h2>
 
-{#if $entities.size > 0}
-    {#each $entities.entries() as [label, resources]}
-        <h3>{plural(label.slice(4))}</h3>
-        {#each resources.entries() as [id, entity]}
-            {#if entity.count}
-                <span
-                    class="entity entitySummary"
-                    typeof={label}
-                    property="sameAs"
-                    resource={id}
-                >
-                    {entity.name}
-                </span>
-            {/if}
-        {/each}
+{#if $classes.size > 0}
+  {#each $classes as [label, resources]}
+    <h3>{plural(label.split(":")[1])}</h3>
+    {#each resources as resource}
+      {#if resource.count}
+        <button
+          type="button"
+          class="entity entitySummary"
+          resource={resource.id}
+          typeof={label}
+          draggable="true"
+          on:dragstart={dragStart}
+          on:dragover={dragOver}
+          on:drop={handleDrop}
+        >
+          {resource.name}
+        </button>
+      {/if}
     {/each}
+  {/each}
 {/if}
+
+<!-- {#if showDropdown} -->
+<!--     <div -->
+<!--         class="dropdown" -->
+<!--         style="position: absolute; left: {dropdownPosition.x}px; top: {dropdownPosition.y}px;" -->
+<!--     > -->
+<!--         {#each getOptions() as relation} -->
+<!--             <button on:click={handleRelationChoice(relation)}>{relation}</button -->
+<!--             > -->
+<!--         {/each} -->
+<!--     </div> -->
+<!-- {/if} -->
