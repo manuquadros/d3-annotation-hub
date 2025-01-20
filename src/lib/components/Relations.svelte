5084a05894f515d378674804305a55a1de7f09a9
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-01-20 15:09:21 +0100

    refactor: focus on subjects when presenting the relations summary

--- src/lib/components/Relations.svelte
+++ src/lib/components/Relations.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { getContext } from "svelte";
     import { displayPredicate } from "$lib/relations.svelte.ts";
+    import { isStrain } from "$lib/resources.svelte.ts";
     import type { bodyStore } from "$lib/body.svelte.ts";
 
     const body: bodyStore = getContext("body");
@@ -9,21 +10,31 @@
 {#if body.relations.size}
     <h2>Relations</h2>
 
-    {#each body.relations.predicates as predicate}
+    {#each body.resources.values() as resource}
         <div class="relations-summary">
-            {#each body.relations.subset({ predicate }) as { subject, object }}
-                <div class="relation">
-                    <span class="subject">
-                        {subject.name}
-                    </span>
-                    <span class="predicate">
-                        {displayPredicate(predicate)}
-                    </span>
-                    <span class="object">
-                        {object.name}
-                    </span>
-                </div>
-            {/each}
+            {#if isStrain(resource)}
+                {@const triples = body.relations.subset({ subject: resource })}
+                {#if triples.size}
+                    <div style="width: 100%; display: table;">
+                        <div style="display: table-row">
+                            <div class="subject">
+                                {resource.name}
+                            </div>
+
+                            <div class="relations">
+                                {#each triples as { subject, predicate, object }}
+                                    <div class="predicate">
+                                        {displayPredicate(predicate)}
+                                        <span class="object">
+                                            {object.name}
+                                        </span>
+                                    </div>
+                                {/each}
+                            </div>
+                        </div>
+                    </div>
+                {/if}
+            {/if}
         </div>
     {/each}
 {/if}
