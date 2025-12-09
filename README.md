d363b17f8215685f2bfbbcadefd7708487ccd2b8
Emanuel Quadros <emanuel.quadros@dsmz.de>
2025-12-09 13:10:17 +0100

    refactor: make the setting of D3HUB_PKPATH more robust
    The variable can be set either as an environment variable or in the
    authentication table of config.toml

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Sample config.toml
 [authentication]
 access_token_expire_minutes = 15
 algorithm = "EdDSA"
+D3HUB_PKPATH = "~/.ssh/d3annotation" # although this can also be set as an env variable
 ```
 
 ### Private key for JSON Web Tokens
 
-The [config](src/ahbackend/config/__init__.py) searches for a private key in the location defined by the environment variable `D3HUB_PKPATH`. You have to set this variable.
+The [config](src/ahbackend/config/__init__.py) searches for a private key in the location defined by `D3HUB_PKPATH`. You have to set this variable either in config.toml as above or as an environment variable in your system.
--- src/ahbackend/config/__init__.py
+++ src/ahbackend/config/__init__.py
@@ -10,22 +10,24 @@ CONFIG_DIR = pathlib.Path(__file__).parent
 
 with pathlib.Path("config.toml").open(mode="r") as cfg:
     config = tomlkit.load(cfg)
-    ALGORITHM = config["authentication"]["algorithm"]
-    ACCESS_TOKEN_EXPIRE_MINUTES = config["authentication"][
+    ALGORITHM = config.value["authentication"]["algorithm"]
+    ACCESS_TOKEN_EXPIRE_MINUTES = config.value["authentication"][
         "access_token_expire_minutes"
     ]
+    PK_PATH: str | None = config.value["authentication"].get(
+        "D3HUB_PKPATH"
+    ) or os.getenv("D3HUB_PKPATH")
 
-PKPATH = os.getenv("D3HUB_PKPATH")
-
-try:
-    with pathlib.Path(PKPATH).open(mode="rb") as sec:
-        private_key_bytes = sec.read()
-        PRIVATE_KEY = serialization.load_ssh_private_key(
-            private_key_bytes, password=None
-        )
-except TypeError:
-    print("You must set the D3HUB_PKPATH.")
-    raise
+if PK_PATH is None:
+    raise RuntimeError(
+        "Please set D3HUB_PKPATH as an environment variable or as a field in "
+        "the authentication table in config.toml"
+    )
+with pathlib.Path(PK_PATH).expanduser().open(mode="rb") as sec:
+    private_key_bytes = sec.read()
+    PRIVATE_KEY = serialization.load_ssh_private_key(
+        private_key_bytes, password=None
+    )
 
 with (CONFIG_DIR / "d3annotation.pub").open(mode="rb") as pubkey_file:
     public_key_bytes = pubkey_file.read()
